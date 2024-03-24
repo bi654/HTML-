4b91d6ef8b4895cb4e3b7edc226f716c7af648c8
bi654 <148889321+bi654@users.noreply.github.com>
2024-03-25 08:37:44 +0900

    Add files via upload

--- 飲食店/script.js
+++ 飲食店/script.js
@@ -1,11 +1,10 @@
 function showDescription(item, description) {
-    var descriptionElement = document.getElementById('item-description');
-    descriptionElement.innerHTML = '<strong>' + item + '</strong>: ' + description;
-  }
-  
-  // ページ読み込み時に初期化
-  window.onload = function () {
-    var descriptionElement = document.getElementById('item-description');
-    descriptionElement.innerHTML = 'メニューアイテムにマウスを重ねて詳細を表示します。';
-  };
-  
+  var descriptionElement = document.getElementById('item-description');
+  descriptionElement.innerHTML = '<strong>' + item + '</strong>: ' + description;
+}
+
+// ページ読み込み時に初期化
+window.onload = function () {
+  var descriptionElement = document.getElementById('item-description');
+  descriptionElement.innerHTML = 'メニューアイテムにマウスを重ねて詳細を表示します。';
+};
